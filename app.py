e1caa74c20973812695f5583afa3c9eef09a861a
StrangelySafeNova <ericfischery@gmail.com>
2025-11-15 14:07:35 -0500

    Fix nav bar in dashboard

--- app.py
+++ app.py
@@ -181,4 +181,4 @@ def mapps():
 
 
 if __name__ == '__main__':
-    app.run( port=80,debug=True)
+    app.run(debug=True)
--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -3,6 +3,20 @@ const sidebarUl = document.getElementById('sidebarUl');
 let currentTab = document.getElementById('tab-dashboard');
 const content = document.getElementById('content');
 
+const tabDashboard = document.getElementById('tab-dashboard');
+const tabAnalytics = document.getElementById('tab-analytics');
+const tabThreats = document.getElementById('tab-threats');
+const tabDevices = document.getElementById('tab-devices');
+const tabServers = document.getElementById('tab-servers');
+const tabLiveMap = document.getElementById('tab-live-map');
+
+const sidebarDashboard = document.getElementById('sidebarDashboard');
+const sidebarAnalytics = document.getElementById('sidebarAnalytics');
+const sidebarThreats = document.getElementById('sidebarThreats');
+const sidebarDevices = document.getElementById('sidebarDevices');
+const sidebarServers = document.getElementById('sidebarServers');
+const sidebarLiveMap = document.getElementById('sidebarLiveMap');
+
 const tabMap = new Map();
 tabMap.set("sidebarDashboard", "tab-dashboard");
 tabMap.set("sidebarAnalytics", "tab-analytics");
@@ -22,15 +36,32 @@ sidebar.addEventListener('mouseleave', () => {
 });
 
 // tab nav
-for (sidebarEl of sidebarUl.children) {
-    sidebarEl.addEventListener('click', () => {
-        console.log('woa');
-        tabId = tabMap.get(sidebarEl);
 
-        currentTab.classList.add('tab-content-hidden');
-        currentTab = document.getElementById(tabId);
-        currentTab.classList.remove('tab-content-hidden');
-    });
+function loadPage(newPage) {
+    currentTab.classList.add('tab-content-hidden');
+    currentTab = newPage;
+    currentTab.classList.remove('tab-content-hidden');
 };
 
+sidebarDashboard.addEventListener('click', () => {
+    loadPage(tabDashboard);
+});
+sidebarAnalytics.addEventListener('click', () => {
+    loadPage(tabAnalytics);
+});
+sidebarThreats.addEventListener('click', () => {
+    loadPage(tabThreats);
+});
+sidebarDevices.addEventListener('click', () => {
+    loadPage(tabDevices);
+});
+sidebarServers.addEventListener('click', () => {
+    loadPage(tabServers);
+});
+sidebarLiveMap.addEventListener('click', () => {
+    loadPage(tabLiveMap);
+});
+
 // threats
+
+
